5f1aaf94f26b182cd0bfb4973b9a88f50afb8146
ruben profit <rubentprofit@gmail.com>
2019-05-31 16:15:07 -0400

    Update header (#3)
    * update header component
    
    replaced ul && li for div
    
    * update header css
    
    updated header styles using flex box
    
    * refactored header
    
    refactored header component and removed unnecessary divs
    
    * fixed spacing
    
    * wrapped header in nav tag and fixed spacing
    
    * added nav-list and nav-item styles

--- src/Header.js
+++ src/Header.js
@@ -3,29 +3,24 @@ import { Link } from "react-router-dom";
 import './Header.css';
 export default class Header extends Component {
   render() {
-    return ( <div className="Header">
-    <h1> {this.props.name}</h1>
-      <div>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Kids</Link>
-              </li>
-              <li>
-                <Link to="/men">Mens</Link>
-              </li>
-              <li>
-                <Link to="/women">Womens</Link>
-              </li>
-              <li>
-                <Link to="/plussize">Plus Size</Link>
-              </li>
-            </ul>
-          </nav>
+    return (
+      <nav className="Header">
+        <h1> {this.props.name}</h1>
+        <div className="nav-list">
+          <div className="nav-item">
+            <Link to="/">Kids</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/men">Mens</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/women">Womens</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/plussize">Plus Size</Link>
+          </div>
         </div>
-      </div>
-      </div>
+      </nav>
     );
   }
 }
