249cfd8931f42b26cc2f16fc833cf56b1abb9458
ruben profit <rubenprofit@ac.c4q.nyc>
2019-06-25 13:02:44 -0400

    updated database sql file
    created users table with seed data

--- backend/DB/sql/database.sql
+++ backend/DB/sql/database.sql
@@ -2,3 +2,15 @@ DROP DATABASE IF EXISTS nalmas-store;
 CREATE DATABASE nalmas-store;
 
 \c nalmas-store;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR NOT NULL, 
+  last_name VARCHAR NOT NULL, 
+  username VARCHAR UNIQUE NOT NULL,
+  password_digest VARCHAR NOT NULL,
+  email VARCHAR UNIQUE NOT NULL,
+  phone VARCHAR UNIQUE NOT NULL
+);
+
+INSERT INTO users (first_name, last_name, username, password_digest, email, phone) VALUES ('Nalma', 'Shaikh', 'naimiii', '$2a$10$q8//j/bkolUGfnNCMl66ge0xTOZiYTX4PQiAkVnwY4A6tk1WXdEVa', 'example@email.com', '5555555555');
