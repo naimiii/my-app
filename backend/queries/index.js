1c52c70bd287d006e3be0621ef976a2bd9376962
ruben profit <rubenprofit@ac.c4q.nyc>
2019-07-10 08:24:56 -0400

    added success and error logic and linted

--- backend/queries/index.js
+++ backend/queries/index.js
@@ -1,11 +1,14 @@
-const {db}=require("../index");
-const fetchUser =(id) =>
+const { db }=require("../index");
+
+export const fetchUser = (id) => 
 {
-    db.any('SELECT * FROM users WHERE active = $1', [true])
-    .then(function(data) {
-        // success;
+    db.one('SELECT * FROM users WHERE active = $1', [true])
+    .then(data => {
+        console.log('our user data:', data);
+        return data.json();
     })
-    .catch(function(error) {
-        // error;
+    .catch(error => {
+        console.log('failed fetching user');
+        throw error;
     });
 };
